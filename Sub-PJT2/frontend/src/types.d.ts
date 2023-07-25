16c5b60daac8a9c39620c632c5d48f43dbb63ce1
Jaemin-Park9708 <catbirdseat9708@gmail.com>
2023-07-25 15:39:46 +0900

    Fix : #S09P12A705-138 박재민 chatRoom 버그 수정.

--- Sub-PJT2/frontend/src/types.d.ts
+++ Sub-PJT2/frontend/src/types.d.ts
@@ -1,14 +1,22 @@
 type UserData = {
-  profileImage: string,
-  name: string,
-  region: string,
-  lastMessage: string,
-  lastMessageTime: string,
-  age: int,
+  profileImage: string;
+  name: string;
+  region: string;
+  lastMessage: string;
+  lastMessageTime: string;
+  age: int;
+  mbti: string;
 };
 
 type Message = {
-  from: string,
-  to: string,
-  content: string,
+  from: string;
+  to: string;
+  content: string;
+  time: string;
+};
+
+type ChatData = {
+  otherUser: UserData;
+  messages: Array<Message>;
+  user: UserData;
 };
