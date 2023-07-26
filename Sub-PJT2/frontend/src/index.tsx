dd6126f4d3a0ff37ca84029f99a0cae560602b3b
HongJaeyeon <ddrrff@kookmin.ac.kr>
2023-07-26 17:22:42 +0900

    feat: msw 추가 및 테스트

--- Sub-PJT2/frontend/src/index.tsx
+++ Sub-PJT2/frontend/src/index.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import Main from "./containers/main/Main";
+
+/* msw - 워커를 실행하는 코드 */
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <RecoilRoot>
     <React.StrictMode>
