f82019f3e13041dba792fcea9a0c582dae57da45
이연주 <legrandreve@naver.com>
2023-07-27 17:33:59 +0900

    Add: S09P12A705-136 이연주 WebRTC 기능 구현

--- Sub-PJT2/frontend/src/containers/onlineMeeting/index.tsx
+++ Sub-PJT2/frontend/src/containers/onlineMeeting/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import profile_pic from './asset/img/profile_pic.png';
+// import profile_pic from './asset/img/profile_pic.png';
 // import ovLogo from './assets/img/openvidu_vert_white_bg_trans_cropped.png';
 // import ovLogo2 from './assets/img/openvidu_logo.png';
 
