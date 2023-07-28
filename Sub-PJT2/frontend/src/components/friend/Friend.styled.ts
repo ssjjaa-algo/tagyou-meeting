3757cfc71fc721a2d52ddf55970ade5cd4d10193
HongJaeyeon <ddrrff@kookmin.ac.kr>
2023-07-28 17:28:00 +0900

    style: 친구 목록 css 수정

--- Sub-PJT2/frontend/src/components/friend/Friend.styled.ts
+++ Sub-PJT2/frontend/src/components/friend/Friend.styled.ts
@@ -2,9 +2,10 @@ import styled from "@emotion/styled";
 import { themeProps } from "@emotion/react";
 import { Link } from "react-router-dom";
 
-export const Container = styled(Link)`
+export const Profile = styled(Link)`
   display: flex;
-  align-content: center;
+  vertical-align: middle;
+  margin-top: 5px;
 `;
 
 export const ProfileImgBox = styled.div`
@@ -14,6 +15,10 @@ export const ProfileImgBox = styled.div`
   width: 80px;
 `;
 
+export const ProfileText = styled.div`
+  margin: auto 0 auto 0;
+`;
+
 export const ProfileImg = styled.img`
   border-radius: 20px;
   object-fit: cover;
@@ -21,4 +26,16 @@ export const ProfileImg = styled.img`
   min-width: 100%;
   min-height: 100%;
   padding: 15px;
+`;
+
+export const Name = styled.div<{ theme: themeProps }>`
+  color: ${(props) => props.theme.font.light};
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const Age = styled(Name)`
+  font-size: 14px;
+  font-weight: normal;
+  margin-top: 10px;
 `;
--- Sub-PJT2/frontend/src/components/friend/index.tsx
+++ Sub-PJT2/frontend/src/components/friend/index.tsx
@@ -1,16 +1,21 @@
 import { friendProps } from "types/types";
 import * as S from "./Friend.styled";
 import tmpImgSrc from "../../asset/img/imgSrcTest.jpg";
+import { themeProps } from "@emotion/react";
+import { useTheme } from "@mui/material";
 
 const Friend = ({ id, name, src }: friendProps) => {
+  const theme: themeProps = useTheme();
   return (
-    <S.Container to="/chatroom">
-      <div>{src}</div>
+    <S.Profile to="/chatroom">
       <S.ProfileImgBox>
         <S.ProfileImg src={tmpImgSrc} width={50} height={50} alt="profile" />
       </S.ProfileImgBox>
-      <span>{name}</span>
-    </S.Container>
+      <S.ProfileText>
+        <S.Name theme={theme}>{name}</S.Name>
+        <S.Age theme={theme}>{name}</S.Age>
+      </S.ProfileText>
+    </S.Profile>
   );
 };
 
