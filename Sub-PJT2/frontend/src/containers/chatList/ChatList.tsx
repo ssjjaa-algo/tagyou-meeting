9e028ef86dfc9fe0bdc9d7395d8036f998bd8d6a
Jaemin-Park9708 <catbirdseat9708@gmail.com>
2023-07-31 10:34:06 +0900

    Fix: 박재민 git 자료 복구

--- Sub-PJT2/frontend/src/containers/chatList/ChatList.tsx
+++ Sub-PJT2/frontend/src/containers/chatList/ChatList.tsx
@@ -1,48 +1,112 @@
-// import { useEffect, useState } from "react";
-import "css/chat/chatList.css";
-import { UserData } from "types/types";
+import { Global } from "@emotion/react";
+import styled from "@emotion/styled";
+import { ThemeProvider } from "@emotion/react";
+import { IsDark } from "../../atoms/atoms";
+import { useRecoilValue } from "recoil";
+import { darkTheme, lightTheme } from "../../styles/ColorSystem";
+import GlobalStyle from "../../styles/GlobalStyle";
+import LeftContainer from "../leftContainer/index";
+import RightContainer from "containers/rightContainer/rightContainer-chatList/ChatList";
+import { UserData, leftContainerProprs } from "types/types";
+import TestImg from "../../asset/img/imgSrcTest.jpg";
 
-interface Listprop {
-  userData: Array<UserData>;
-}
 
-const chatList = ({ userData }: Listprop) => {
-  // const [data, setData] = useState<Array<Data>>();
+function ChatList() {
+  const isDark = useRecoilValue(IsDark);
 
-  // useEffect(() => {
-  //   setData(dataList);
-  // }, [dataList]);
+  const leftContainerData: leftContainerProprs = {
+    imgSrc: TestImg,
+    name: "스티븐",
+    age: 32,
+  };
+
+  const dataList: Array<UserData> = [
+    {
+      profileImage: "pic1",
+      name: "송강",
+      region: "서울 은평구",
+      lastMessage: "ㅎㅇㅎㅇㅎㅇ",
+      lastMessageTime: "2023.07.13 13:56",
+      age: 25,
+      mbti: "ENTP",
+    },
+    {
+      profileImage: "pic2",
+      name: "차은우",
+      region: "경기 군포시",
+      lastMessage: "ㅎㅇㅎㅇㅎㅇ",
+      lastMessageTime: "2023.07.13 14:56",
+      age: 24,
+      mbti: "ENFP",
+    },
+    {
+      profileImage: "pic3",
+      name: "고민시",
+      region: "대전 광역시",
+      lastMessage: "ㅎㅇㅎㅇㅎㅇ",
+      lastMessageTime: "2023.07.13 15:56",
+      age: 23,
+      mbti: "INTP",
+    },
+    {
+      profileImage: "pic4",
+      name: "로운",
+      region: "서울 영등포구",
+      lastMessage: "ㅎㅇㅎㅇㅎㅇ",
+      lastMessageTime: "2023.07.13 17:46",
+      age: 21,
+      mbti: "ISTP",
+    },
+    {
+      profileImage: "pic5",
+      name: "윈터",
+      region: "서울 영등포구",
+      lastMessage: "ㅎㅇㅎㅇㅎㅁㄴㅇㄹㅇ",
+      lastMessageTime: "2023.07.11 15:46",
+      age: 23,
+      mbti: "ENTJ",
+    },
+    {
+      profileImage: "pic6",
+      name: "A",
+      region: "서울 동작구",
+      lastMessage: "ㅎㅇㅎㅇㅎㅇ",
+      lastMessageTime: "2023.07.10 17:46",
+      age: 28,
+      mbti: "ENFJ",
+    },
+    {
+      profileImage: "pic7",
+      name: "B",
+      region: "서울 은평구",
+      lastMessage: "ㅎㅇㅎㅇㅎㅇ",
+      lastMessageTime: "2023.07.09 17:46",
+      age: 30,
+      mbti: "INFP",
+    },
+  ];
 
   return (
-    <div className="chatList">
-      <div className="chatList-body">
-        {userData?.map((item: UserData, idx: number) => (
-          <div className="chat" key={idx}>
-            {/* <img src="" alt="" /> */}
-            <div className="chat-profileImage">{item.profileImage}</div>
-            <div className="chat-content">
-              <div className="chat-content-userData">
-                <div className="chat-content-userData-nameAndAge">
-                  {item.name} | {item.age}
-                </div>
-                <div className="chat-content-userData-region">
-                  {item.region}
-                </div>
-              </div>
-              <div className="chat-content-message">
-                <div className="chat-content-message-content">
-                  {item.lastMessage}
-                </div>
-                <div className="chat-content-message-time">
-                  {item.lastMessageTime}
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+    <div>
+      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <Global styles={GlobalStyle} />
+        <Container>
+        <LeftContainer
+              imgSrc={leftContainerData.imgSrc}
+              name={leftContainerData.name}
+              age={leftContainerData.age}
+            />
+          <RightContainer userData={dataList} />
+        </Container>
+      </ThemeProvider>
     </div>
   );
-};
+}
 
-export default chatList;
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export default ChatList;
---
+++ Sub-PJT2/frontend/src/containers/landing/Landing.tsx
@@ -0,0 +1,14 @@
+import {themeProps} from "@emotion/react";
+import { useTheme } from "@emotion/react";
+import * as S from "./Landing.styled";
+
+
+const Landing = () => {
+  return(
+    <S.Container>
+
+    </S.Container>
+  )
+}
+
+export default Landing;
