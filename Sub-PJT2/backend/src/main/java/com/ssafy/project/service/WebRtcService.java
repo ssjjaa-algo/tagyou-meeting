18c44c2a725aa3b1d86dc0110e429905fd8f1db2
mindy0414 <mmminjung0414@gmail.com>
2023-08-08 11:13:29 +0900

    Add: S09P12A705-329 webRtc 서비스 추가

---
+++ Sub-PJT2/backend/src/main/java/com/ssafy/project/service/WebRtcService.java
@@ -0,0 +1,43 @@
+package com.ssafy.project.service;
+
+import com.ssafy.project.exception.NotFoundException;
+import io.openvidu.java.client.*;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public class WebRtcService {
+
+    @Value("${OPENVIDU_URL}")
+    private String OPENVIDU_URL;
+
+    @Value("${OPENVIDU_SECRET}")
+    private String OPENVIDU_SECRET;
+
+    private OpenVidu openvidu;
+
+    @PostConstruct
+    public void init() {
+        this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+    }
+
+    public String initializeSession(Map<String, Object> params) throws OpenViduJavaClientException, OpenViduHttpException {
+        SessionProperties properties = SessionProperties.fromJson(params).build();
+        Session session = openvidu.createSession(properties);
+        return session.getSessionId();
+    }
+
+    public String createConnection(String sessionId, Map<String, Object> params) throws OpenViduJavaClientException, OpenViduHttpException {
+        Session session = openvidu.getActiveSession(sessionId);
+        if (session == null) {
+            throw new NotFoundException("해당 아이디에 해당에 세션이 없습니다. ");
+        }
+        ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+        Connection connection = session.createConnection(properties);
+        return connection.getToken();
+    }
+
+}
