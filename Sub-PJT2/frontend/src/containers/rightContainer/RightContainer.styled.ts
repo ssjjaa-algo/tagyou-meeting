939344f8fc96a27d86a6116b59d0a45237fbe8f0
이연주 <legrandreve@naver.com>
2023-08-09 15:31:03 +0900

    Fix: #S09P12A705-309 이연주 backgroundcolor css 수정

--- Sub-PJT2/frontend/src/containers/rightContainer/RightContainer.styled.ts
+++ Sub-PJT2/frontend/src/containers/rightContainer/RightContainer.styled.ts
@@ -4,6 +4,7 @@ import { themeProps } from "@emotion/react";
 export const Container = styled.div<{ theme: themeProps }>`
   background-color: ${(props) => props.theme.bg.light};
   transition: width, 500ms;
+  min-height: 100vh;
   margin: auto;
 `;
 
