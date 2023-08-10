26c4f91ca444045443a62e4670c3eeb9f91917e8
이연주 <legrandreve@naver.com>
2023-08-10 09:59:46 +0900

    Fix: 이연주 파일 루트 수정

--- Sub-PJT2/frontend/src/containers/main/Main.tsx
+++ Sub-PJT2/frontend/src/containers/main/Main.tsx
@@ -23,7 +23,8 @@ function Main() {
 
   const showSideBar = () => {
     const position = document.location.pathname;
-    if (position === "/chatList" || position.search("chatRoom") === 1) {
+    if (position === "/chatList" || position.search("chatRoom") === 1
+    || position === "/mypage" || position === "/home") {
       return true;
     }
     return false;
@@ -43,9 +44,8 @@ function Main() {
                 age={leftContainerData.age}
               />
             ) : null}
-            <RightContainer>
               <Router />
-            </RightContainer>
+            <RightContainer/>
           </Container>
         </BrowserRouter>
       </ThemeProvider>
