a633d69a79f6849ddb9c308225007b5a5408a363
mindy0414 <mmminjung0414@gmail.com>
2023-08-10 16:51:52 +0900

    Add: webRtc -ing

--- Sub-PJT2/backend/src/main/java/com/ssafy/project/service/WebRtcService.java
+++ Sub-PJT2/backend/src/main/java/com/ssafy/project/service/WebRtcService.java
@@ -3,13 +3,22 @@ package com.ssafy.project.service;
 import com.ssafy.project.exception.NotFoundException;
 import io.openvidu.java.client.*;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class WebRtcService {
+
+    private Map<String, Session> sessions = new ConcurrentHashMap<>();
 
     @Value("${OPENVIDU_URL}")
     private String OPENVIDU_URL;
@@ -24,22 +33,30 @@ public class WebRtcService {
         this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
     }
 
-    public String initializeSession(Map<String, Object> params) throws OpenViduJavaClientException, OpenViduHttpException {
-        SessionProperties properties = SessionProperties.fromJson(params).build();
-        Session session = openvidu.createSession(properties);
+    // 방 만들기 : 방장 혹은 관리자(이렇게 하면 한명 제외하고 띄워야 함)
+    public String initializeSession() throws OpenViduJavaClientException, OpenViduHttpException {
+        Session session = openvidu.createSession();
+        sessions.put(session.getSessionId(), session);
         return session.getSessionId();
     }
 
-    public String createConnection(String sessionId, Map<String, Object> params) throws OpenViduJavaClientException, OpenViduHttpException {
-        Session session = openvidu.getActiveSession(sessionId);
-        if (session == null) {
+    public String createConnection(String sessionId) throws OpenViduJavaClientException, OpenViduHttpException {
+//        Session session = openvidu.getActiveSession(sessionId);
+        Session tmpSes = sessions.get(sessionId);
+
+        if (tmpSes == null) {
             throw new NotFoundException("해당 아이디에 해당에 세션이 없습니다. ");
         }
-        ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
         // 들어오는 사람의 권한이 publisher. role()을 어디서 만지지??
-        // params를 여기서 생성하면 되지 않을까?
-        Connection connection = session.createConnection(properties);
-        return connection.getToken();
+        OpenViduRole role = OpenViduRole.PUBLISHER; // OpenViduRole.SUBSCRIBER;
+        TokenOptions tokenOpts = new TokenOptions.Builder()
+                                        .role(role)
+                                        .data("SERVER= participant num").build();
+        String token = tmpSes.generateToken(tokenOpts);
+
+        return token;
+//        Connection connection = session.createConnection();
+//        return connection.getToken();
     }
 
 }
