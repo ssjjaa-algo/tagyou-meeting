3d32166e2263309fa39a99a3c48cb48b30f59ada
HongJaeyeon <ddrrff@kookmin.ac.kr>
2023-08-14 14:01:23 +0900

    fix: 친구 요청 기능 수정

--- Sub-PJT2/frontend/src/components/modal/index.tsx
+++ Sub-PJT2/frontend/src/components/modal/index.tsx
@@ -2,6 +2,8 @@ import * as S from "./modal.styled";
 import kakaoImg from "../../asset/img/kakao_login_medium_wide.png";
 import logoImg from "../../asset/img/logo/2.png";
 import { useCookies } from "react-cookie";
+import { useRecoilState } from "recoil";
+import { TokenValue } from "atoms/atoms";
 
 type modalProps = {
   handleOnClick: () => void;
@@ -19,9 +21,10 @@ export const Modal = ({
   };
 
   const [, , removeCookie] = useCookies(["Auth"]);
-
+  const [token, setToken] = useRecoilState(TokenValue);
   const handleLogout = () => {
     removeCookie("Auth");
+    setToken("");
     window.location.href = "/";
   };
 
