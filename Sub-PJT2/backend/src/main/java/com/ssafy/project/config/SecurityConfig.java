6dc649a40db8d1267b174107534d3ee6204d30a6
조태규 <hey03312@gmail.com>
2023-08-14 13:43:29 +0900

    add : OneMeetingRoom 완성, user 조회하여 확인 & SwaggerConfig 구현

--- Sub-PJT2/backend/src/main/java/com/ssafy/project/config/SecurityConfig.java
+++ Sub-PJT2/backend/src/main/java/com/ssafy/project/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -52,5 +53,15 @@ public class SecurityConfig {
                 .userInfoEndpoint().userService(oAuth2UserService);
         http.addFilterBefore(new JwtAuthFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return web -> {
+            web.ignoring()
+                    .requestMatchers(
+                            "api/swagger-ui/**"
+                    );
+        };
     }
 }
