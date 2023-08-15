a610e5767bc29e5c4f70776f1de75d580aaa1e77
mindy0414 <mmminjung0414@gmail.com>
2023-08-15 14:35:48 +0900

    Fix: security config migration done

--- Sub-PJT2/backend/src/main/java/com/ssafy/project/config/SecurityConfig.java
+++ Sub-PJT2/backend/src/main/java/com/ssafy/project/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -28,12 +30,28 @@ public class SecurityConfig {
 
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.csrf(AbstractHttpConfigurer::disable)
-//                .sessionManagement((sessionManagement) ->
-//                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                ).authorizeHttpRequests((authorizeRequests) ->
-//                        authorizeRequests.anyRequest().permitAll()
-//                );
+        http
+                .httpBasic(HttpBasicConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers("/swagger-resources/**").permitAll()
+                                .requestMatchers("/swagger-ui/**").permitAll() // 스웨거 경로 허용
+                                .requestMatchers("/ws/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(corsConfig.corsFilter(), UsernamePasswordAuthenticationFilter.class) // CorsConfig를 먼저 실행하도록 변경
+                .addFilterBefore(new JwtAuthFilter(tokenService), CorsFilter.class) // JwtAuthFilter를 CorsFilter 뒤로 이동
+                .addFilterBefore(exceptionHandlerFilter, JwtAuthFilter.class)
+                .oauth2Login(oauth2Login ->
+                                oauth2Login
+                                        .successHandler(successHandler)
+                                        .userInfoEndpoint(userInfoEndpointConfig
+                                                -> userInfoEndpointConfig.userService(oAuth2UserService))
+                );
 
 //        http.httpBasic().disable()
 //                .csrf().disable()
@@ -55,27 +73,28 @@ public class SecurityConfig {
 //                .userInfoEndpoint().userService(oAuth2UserService);
 //        http.addFilterBefore(new JwtAuthFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
 
-        http.httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-//                                .requestMatchers("/api/token/**", "/api/login/oauth2/kakao").permitAll()
-                                .requestMatchers("/swagger-resources/**").permitAll()
-                                .requestMatchers("/swagger-ui/**").permitAll() // 스웨거 경로 허용
-                                .requestMatchers("/ws/**").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .addFilterBefore(corsConfig.corsFilter(), UsernamePasswordAuthenticationFilter.class) // CorsConfig를 먼저 실행하도록 변경
-                .addFilterBefore(new JwtAuthFilter(tokenService), CorsFilter.class) // JwtAuthFilter를 CorsFilter 뒤로 이동
-                .addFilterBefore(exceptionHandlerFilter, JwtAuthFilter.class)
-                .oauth2Login(oauth2Login ->
-                        oauth2Login
-//                                .loginPage("http://localhost:3000/home")
-                                .successHandler(successHandler)
-                                .userInfoEndpoint().userService(oAuth2UserService)
-                );
+//        http
+//                .httpBasic().disable()
+//                .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authorizeRequests(authorizeRequests ->
+//                        authorizeRequests
+////                                .requestMatchers("/api/token/**", "/api/login/oauth2/kakao").permitAll()
+//                                .requestMatchers("/swagger-resources/**").permitAll()
+//                                .requestMatchers("/swagger-ui/**").permitAll() // 스웨거 경로 허용
+//                                .requestMatchers("/ws/**").permitAll()
+//                                .anyRequest().authenticated()
+//                )
+//                .addFilterBefore(corsConfig.corsFilter(), UsernamePasswordAuthenticationFilter.class) // CorsConfig를 먼저 실행하도록 변경
+//                .addFilterBefore(new JwtAuthFilter(tokenService), CorsFilter.class) // JwtAuthFilter를 CorsFilter 뒤로 이동
+//                .addFilterBefore(exceptionHandlerFilter, JwtAuthFilter.class)
+//                .oauth2Login(oauth2Login ->
+//                        oauth2Login
+////                                .loginPage("http://localhost:3000/home")
+//                                .successHandler(successHandler)
+//                                .userInfoEndpoint().userService(oAuth2UserService)
+//                );
 
         return http.build();
     }
