e4236bab1e1f3f50b8c84a664849c2e271d5b96f
조태규 <hey03312@gmail.com>
2023-08-15 20:15:52 +0900

    fix : 도메인 yml 주소로 바꿈

--- Sub-PJT2/backend/src/main/java/com/ssafy/project/repository/OneRoomRepository.java
+++ Sub-PJT2/backend/src/main/java/com/ssafy/project/repository/OneRoomRepository.java
@@ -6,9 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OneRoomRepository extends JpaRepository<OneMeetingRoom, Long>, OneRoomRepositoryCustom{
-
     Optional<OneMeetingRoom> findById(Long id);
-    Optional<OneMeetingRoom> findByMaleUserId(Long userId);
-
-    Optional<OneMeetingRoom> findByFemaleUserId(Long userId);
 }
