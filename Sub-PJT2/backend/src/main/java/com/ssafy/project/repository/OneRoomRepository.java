35acfffc750211aad86b88f22744601bd4af54e0
조태규 <hey03312@gmail.com>
2023-08-17 10:29:37 +0900

    fix : 임시 Room 컨트롤러 유저 볼 수 있게 설정

--- Sub-PJT2/backend/src/main/java/com/ssafy/project/repository/OneRoomRepository.java
+++ Sub-PJT2/backend/src/main/java/com/ssafy/project/repository/OneRoomRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OneRoomRepository extends JpaRepository<OneMeetingRoom, Long>, OneRoomRepositoryCustom{
-    Optional<OneMeetingRoom> findById(Long id);
 }
