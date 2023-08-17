c7422d2df8806ee6c89b1beffc6376396b4ad8d7
Jaemin-Park9708 <catbirdseat9708@gmail.com>
2023-08-17 16:51:23 +0900

    Fix: 박재민 - UserStatus 불안정 상태 부분해소

--- Sub-PJT2/frontend/src/containers/rightContainer/index.tsx
+++ Sub-PJT2/frontend/src/containers/rightContainer/index.tsx
@@ -1,10 +1,7 @@
 import * as S from "./RightContainer.styled";
 import { themeProps } from "@emotion/react";
 import { useTheme } from "@mui/material";
-import { TokenValue } from "atoms/atoms";
-import { ReactNode, useEffect, useState } from "react";
-import { Cookies } from "react-cookie";
-import { useRecoilState } from "recoil";
+import { ReactNode} from "react";
 
 type RightContinaerProp = {
   children?: JSX.Element | JSX.Element[] | ReactNode;
